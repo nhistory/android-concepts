ee26fd36e9dd629aef6fdf3d9b7ad06c6e0e12db
nhistory <nhistory@naver.com>
2022-08-10 00:06:32 -0300

    Check error message by logcat

--- app/src/main/java/com/example/android_concepts/MainActivity.kt
+++ app/src/main/java/com/example/android_concepts/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.android_concepts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = listOf(5,3,6,7,9,1)
+        sortList(list)
+        println(list)
     }
+
+    private fun sortList(list: List<Int>) {
+        for (i in 0..list.size - 1) {
+            for (j in 0..list.size - 2) {
+                if(list[j] > list[j+1]) {
+                    Collections.swap(list, j, j+1)
+                }
+            }
+        }
+    }
+
 }
