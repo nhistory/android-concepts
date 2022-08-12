8564bbd37560e138978866dabbb8f45f7c6f8321
nhistory <nhistory@naver.com>
2022-08-12 01:32:35 -0300

    Create linear layout and test edittext and button

--- app/src/main/java/com/example/android_concepts/MainActivity.kt
+++ app/src/main/java/com/example/android_concepts/MainActivity.kt
@@ -5,23 +5,9 @@ import android.os.Bundle
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val list = listOf(5,3,6,7,9,1)
-        sortList(list)
-        println(list)
     }
-
-    private fun sortList(list: List<Int>) {
-        for (i in 0..list.size - 1) {
-            for (j in 0..list.size - 2) {
-                if(list[j] > list[j+1]) {
-                    Collections.swap(list, j, j+1)
-                }
-            }
-        }
-    }
-
 }
