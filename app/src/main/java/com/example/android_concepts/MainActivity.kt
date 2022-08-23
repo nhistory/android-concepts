c3e2ea26c26b33015bbd1d76b56e0b2d633d5165
nhistory <nhistory@naver.com>
2022-08-22 22:37:08 -0300

    Make button click listener with EditText input text.

--- app/src/main/java/com/example/android_concepts/MainActivity.kt
+++ app/src/main/java/com/example/android_concepts/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.android_concepts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnApply.setOnClickListener{
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val birthDate = etBirthDate.text.toString()
+            val country = etCountry.text.toString()
+            Log.d("MainActivity", "$firstName $lastName, born on $birthDate, from $country just applied to the formular.")
+        }
     }
 }
