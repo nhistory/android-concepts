a2b6f74149f7f0ecaf3b00eb159ec4e46516b2da
nhistory <sehwan.lee.dev@gmail.com>
2022-09-01 00:51:31 -0300

    Make new activity and change with context intent

---
+++ app/src/main/java/com/example/android_concepts/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android_concepts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
